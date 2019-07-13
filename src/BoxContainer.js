5328f6c7bc43eabd93d80f769774edaab67af1da
Moize <moize.ahsan@hotmail.com>
2019-07-13 15:34:33 +0500

    add selection

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import { Container, Jumbotron, Button, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Box extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedIndex: null
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(idx) {
+        this.setState(() => {
+            return {
+                selectedIndex: idx
+            }
+        })
+    }
     render() {
         const answerList = this.props.answerList ? this.props.answerList.map((x, idx) => {
             return (
-                <ListGroupItem key={idx}>{x}</ListGroupItem>
+                <ListGroupItem
+                    key={idx}
+                    active={this.state.selectedIndex === null ? false : this.state.selectedIndex === idx ? true : false}
+                    onClick={() => { this.handleClick(idx) }}>{x}</ListGroupItem>
             )
         }) : <h1> Loading...</h1>
         return (
